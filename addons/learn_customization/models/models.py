cf60e57342ea8c3cb187c78e6dd4000416109164
Gian Gianna <gian.ule@gmail.com>
2024-03-19 10:59:32 +0700

    Customizing existing model

--- addons/learn_customization/models/models.py
+++ addons/learn_customization/models/models.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class learn_customization(models.Model):
-#     _name = 'learn_customization.learn_customization'
-#     _description = 'learn_customization.learn_customization'
+class EstatePropertyTagInherit(models.Model):
+    _inherit = 'estate.property.tag'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    priority = fields.Integer(string="Tag Priority", default=0)
+    color = fields.Integer(string="Color Index Overide field", required=True)
+
+    # def create(self):
+    # 	# put your codes here
+    #     return super().create()
+
+class SalesOrderInherit(models.Model):
+    _inherit = "sale.order"
+
+    client_document_number = fields.Char(string="SO Client Number")
 
